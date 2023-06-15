07be4733c2ce792d21a519cd547f08590b0e4f19
han16nah <han_nah@mailbox.org>
2023-06-15 14:01:47 +0200

    update leaf metrics script

--- addons/leaf_metrics.py
+++ addons/leaf_metrics.py
@@ -7,32 +7,42 @@ import bpy
 import bmesh
 
 
-#def get_leaf_angles(leaf_objects):
-#
-#    zeniths = np.empty(shape=(len(leaf_objects),))
-#    azimuths = np.empty(shape=(len(leaf_objects),))
-#
-#    # get normal vectors of all leaves
-#    for i, leaf in enumerate(leaf_objects):
-#        assert(leaf.type == 'MESH')
+def get_leaf_angles(leaf_objects):
 
-#        # get normal vector
-#        # any face from the mesh will do, as leaves are all planar, so let's just get the first
-#        normal_vec = leaf.data.polygons[0].normal.to_4d()
-#        normal_vec.w = 0
-#        normal_vec = (leaf.matrix_world @ normal_vec).to_3d()
-    
-#        # get zenith angle
-#        ang_zenith = math.degrees(normal_vec.angle(Vector((0,0,1))))
-#        ang_azimuth = math.degrees(normal_vec.angle(Vector((0,1,0))))
-#    
-#        if ang_zenith > 90:
-#            ang_zenith = 180-ang_zenith
+    zeniths = np.empty(shape=(len(leaf_objects),))
+    azimuths = np.empty(shape=(len(leaf_objects),))
 
-#        zeniths[i] = ang_zenith
-#        azimuths[i] = ang_azimuth
+    # get normal vectors of all leaves
+    for i, leaf in enumerate(leaf_objects):
+        assert(leaf.type == 'MESH')
+        print(f"leaf {i}")
+
+        # get normal vector
+        # any face from the mesh will do, as leaves are all planar, so let's just get the first/second
+        normal_vec = leaf.data.polygons[1].normal.to_4d()
+        normal_vec.w = 0
+        normal_vec = (leaf.matrix_world @ normal_vec).to_3d()
     
-#    return zeniths, azimuths
+        # get zenith angle
+        try:
+            ang_zenith = math.degrees(normal_vec.angle(Vector((0,0,1))))
+        except ValueError as e:
+            print(e)
+            print("Zenith angle could not be estimated")
+            ang_azimuth = np.nan
+        try:
+            ang_azimuth = math.degrees(normal_vec.angle(Vector((0,1,0))))
+        except ValueError as e:
+            print(e)
+            print("Azimuth angle could not be estimated")
+            ang_zenith = np.nan
+        if ang_zenith > 90:
+            ang_zenith = 180-ang_zenith
+
+        zeniths[i] = ang_zenith
+        azimuths[i] = ang_azimuth
+    
+    return zeniths, azimuths
 
 
 def get_leaf_area(leaf_objects):
@@ -54,6 +64,10 @@ def get_leaf_area(leaf_objects):
 
 if __name__ == "__main__":
     
+    output_directory = "H:/movingtree_b2h/data/metrics"
+    project_name = Path(bpy.data.filepath).stem
+    tree_id = project_name.split("_")[1].replace("tree", "")
+
     # get all leaves
     scene = bpy.context.scene
     objects = scene.objects
@@ -61,15 +75,30 @@ if __name__ == "__main__":
     leaves = []
     for o in objects:
         if o.name.startswith("leaves") or o.name.startswith("Leaves"):
-        # if o.name.startswith("Plane"):
             leaves.append(o)
+    
+    old_current_frame = scene.frame_current
 
-    # zenith_angles, azimuth_angles = get_leaf_angles(leaves)
+    while bpy.ops.screen.keyframe_jump(next=True) != {'CANCELLED'}:
+        pass
+
+    frame_end = scene.frame_current
+    print(frame_end)
+    scene.frame_current = old_current_frame
     
-    output_directory = "H:/movingtree_b2h/data"
+    timestamps = np.arange(frame_end+5, step=5)
     
-    project_name = Path(bpy.data.filepath).stem
-    tree_id = project_name.split("_")[1].replace("tree", "")
+    for t in timestamps:
+        t = int(t)
+        # move forward by duration
+        scene.frame_set(t)
+        
+        zenith_angles, azimuth_angles = get_leaf_angles(leaves)
+        
+        # save to file to later analyze in notebook
+        np.savetxt(Path(output_directory) / f"{project_name}_leaf_zenith_angles_{t}.txt", zenith_angles, fmt='%.8f')
+        np.savetxt(Path(output_directory) / f"{project_name}_leaf_azimuth_angles_{t}.txt", azimuth_angles, fmt='%.8f')
+        
     
     # save to file to later analyze in notebook
     # np.savetxt(Path(output_directory) / f"{project_name}_leaf_zenith_angles.txt", zenith_angles, fmt='%.8f')
@@ -77,9 +106,9 @@ if __name__ == "__main__":
 
     leaf_areas = get_leaf_area(leaves)
     
-    with open("H:/movingtree_b2h/data/metrics/leaf_area_reference.txt", "a") as f:
+    with open(Path(output_directory) / "leaf_area_reference.txt", "a") as f:
         f.write(f"{tree_id} {np.sum(leaf_areas):.8f}\n")
     
-    # print(f"Number of leaves: {len(leaves)}")
-    # print(f"{np.sum(leaf_areas):.4f} m2")
-    # print(f"Mean leaf area: {np.mean(leaf_areas):.4f} m2")
+    print(f"Number of leaves: {len(leaves)}")
+    print(f"{np.sum(leaf_areas):.4f} m2")
+    print(f"Mean leaf area: {np.mean(leaf_areas):.4f} m2")
